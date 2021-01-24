92d6a7f5fa08d26507691c4f1a11a17908c6fca5
LH <bofhik42@seznam.cz>
2021-01-24 20:48:51 +0100

    OLED display AND interrupts doesn't work :(

--- Well_spill_flowmeter/src/cfg.h
+++ Well_spill_flowmeter/src/cfg.h
@@ -1,1 +1,5 @@
-#define FLOW_SENSOR_PIN 13  // D7
+#define FLOW_SENSOR_PIN 0
+
+#define OLED_SDA 2
+#define OLED_SCL 14
+#define OLED_RST 4
--- Well_spill_flowmeter/src/main.cpp
+++ Well_spill_flowmeter/src/main.cpp
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+#include <SPI.h>
+
 #include "cfg.h"
 #include "wifi_cfg.h"
 
@@ -6,6 +10,7 @@ float flow_scale = 1;
 volatile unsigned int pulse_count = 0;
 float flow_rate = 0.0;
 unsigned long startTime = 0;
+U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, OLED_SCL, OLED_SDA , OLED_RST);
 
 
 void incrementCounter() {  // IRQ handler
@@ -13,28 +18,36 @@ void incrementCounter() {  // IRQ handler
 }
 
 void setup() {
-  Serial.begin(19200);
+  Serial.begin(115200);
+  u8g2.begin();
   delay(500);
+
   Serial.println();
   Serial.println("Sensor init..");
 
   pinMode(FLOW_SENSOR_PIN, INPUT_PULLUP);
 
-# TODO - IRQs doesn't work 
-  attachInterrupt(digitalPinToInterrupt(FLOW_SENSOR_PIN), incrementCounter, FALLING);
+  String sOut = "mikrofon";
+  u8g2.clearBuffer(); // clear the internal memory
+  u8g2.setFont(u8g2_font_8x13B_mf);
+  u8g2.drawStr(20,20,sOut.c_str());
+  u8g2.sendBuffer();
+
+  // attachInterrupt(digitalPinToInterrupt(FLOW_SENSOR_PIN), incrementCounter, FALLING);
 
 }
 
 void loop() {
-  if ((millis() - startTime) > 1000) {
-    noInterrupts();
-    flow_rate = flow_scale * pulse_count / (millis() - startTime);
-    Serial.print("Current flow: ");
-    Serial.print(flow_rate, 1);
-    Serial.println(" l / min");
-    pulse_count = 0;
-    startTime = millis();
-    interrupts();
-
-  }
+  Serial.println(pulse_count);
+  delay(200);
+  // if ((millis() - startTime) > 1000) {
+  //
+  //   flow_rate = flow_scale * pulse_count / (millis() - startTime);
+  //   Serial.print("Current flow: ");
+  //   Serial.print(flow_rate, 1);
+  //   Serial.println(" l / min");
+  //   pulse_count = 0;
+  //   startTime = millis();
+  //
+  // }
 }
